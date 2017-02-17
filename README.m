647f80b04cf2aec78e70d8329e7e02d6f11ada8c
Alind Gupta <alindgupta19@gmail.com>
2017-02-17 16:28:10 -0500

    Modified README.m

---
+++ README.m
@@ -0,0 +1,28 @@
+ctive multi-document summarization models in Python
+
+Description TODO
+
+## Installation
+
+TODO: Describe the installation process
+
+## Usage
+
+TODO
+
+```python
+pass
+```
+
+## Contributing
+
+TODO
+
+
+## Credits
+
+TODO: Write credits
+
+## License
+
+TODO: Write license
